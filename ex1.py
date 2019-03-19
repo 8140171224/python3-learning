1b17189a0c91d8e178b4e5e8d09a64c3db8ba1ea
8140171224 <45361041+8140171224@users.noreply.github.com>
2019-03-19 12:56:57 +0530

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,26 @@
+from sys import argv
+	
+script, user_name = argv
+prompt = '>+'
+	
+print(f"Whats your name??")
+user_name = input(prompt)
+
+print(f"Hi {user_name}, I'm the {script} script")
+print("I'd like to ask you a few questions.")
+
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print(f"Whice Kind of computer do you have {user_name}?")
+computer = input(prompt)
+
+print(f'''
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.
+''')
+
