3ed848529ddaaf5722aa75708946bd9059cb96f8
Aakash Padhiyar <45361041+8140171224@users.noreply.github.com>
2019-03-23 11:20:57 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,15 @@
+#strings
+
+# They can enclose in single qoutes ('...') or double quotes ("...") with the same result. \ can be used to escape quotes:
+
+print('spam eggs')  # single quotes
+
+print('doesn\'t')  # use \' to escape the single quote...
+
+print("doesn't")   # ....or use double quotes instead
+
+print('"YES," they said.')
+
+print("\"YES,\" they said.")
+
+print('"Ist\'t" they said."')
