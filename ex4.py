e0ffdef2993eae96cd7ca37470ad292b4c13ccd2
Aakash Padhiyar <45361041+8140171224@users.noreply.github.com>
2019-03-25 16:29:45 +0530

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,26 @@
+# The range() Function Few example.
+for i in range(6):
+    print(i)
+
+
+# below line '\n' means new lines
+print('\n')
+
+
+#range(5, 10)
+for i in range(5, 10):
+    print(i)
+
+# below line '\n' means new lines
+print('\n')
+
+#range(0, 20, 2)
+for i in range(0, 22, 2):
+    print(i)
+
+# below line '\n' means new lines
+print('\n')
+
+#range(-20, -200, -30)
+for i in range(-20, -100, -30):
+    print(i)
